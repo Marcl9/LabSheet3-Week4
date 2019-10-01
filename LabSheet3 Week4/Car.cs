2f6d8a3c6dba9803cb872555b462b10fc8a69efb
Emmanuel Olagbaju <S00176880@mail.itsligo.ie>
2019-10-01 12:54:51 +0100

    Add project files.

---
+++ LabSheet3 Week4/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSheet3_Week4
+{
+    public class Car
+    {
+        private string Make;
+
+        public string Make
+        {
+            get { return this.Make; }
+            set { this.Make = Value; }
+        }
+
+    }
+}
---
+++ LabSheet3 Week4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSheet3_Week4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c1 = new Car();
+            c1.make = "Ford";
+            c1.Model = "Focus";
+            c1.CurrentSpeed = 0;
+
+
+        }
+    }
+}
